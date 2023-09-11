6f5a5ef2acaeaede3b845569497f0754ff1f350b
Marisol Garrouste (INL) <marisol.garrouste@inl.gov>
2023-09-11 09:51:58 -0400

    Concat all results in same csv file

--- use_cases/LWR_FT_2023/run/compute_tallies.py
+++ use_cases/LWR_FT_2023/run/compute_tallies.py
@@ -13,6 +13,7 @@ FUEL_DENSITY = {'jet_fuel':0.8,
                 'diesel':0.85,
                 'naphtha':0.77} # kg/L
 GAL_to_L = 3.785 # L/gal
+locations =['braidwood', 'cooper', 'davis_besse', 'prairie_island', 'stp']
 
 def parse_meta(xml_file):
   """ Parse the ROM meta xml file to get the cluster multiplicity information
@@ -161,9 +162,26 @@ def plot_lifetime_tallies(lifetime_series, lifetime_std, save_path):
   fig.savefig(save_path)
   return None
 
+
+def combine_tallies():
+  total_df = pd.DataFrame()
+  counter=0
+  for loc in locations:
+    df_loc = pd.read_csv(os.path.join(loc+'_dispatch', loc+'_lifetime_tallies.csv'))
+    df_loc.set_axis(['tally', loc], axis=1, inplace=True)
+    df_loc.sort_values(by=['tally'], inplace=True)
+    if counter == 0: 
+      total_df['tally'] = df_loc['tally']
+    total_df[loc] = df_loc[loc]
+    counter += 1
+  total_df.to_csv('all_locations_tallies.csv', index=False)
+
+
+
 def main():
   parser = argparse.ArgumentParser()
   parser.add_argument("location", type=str, help="Name of NPP location to compute tallies for")
+  parser.add_argument("-c", "--combine", help="Combine tallies", type=bool, required=False, default=False)
   args = parser.parse_args()
   
   #Check dispatch result file
@@ -186,6 +204,7 @@ def main():
     exit(f'No golded dispatch results here : {gold}')
   else: 
     print(f'Dispatch results found here : {gold}')
+
   # Compute tallies for the duration of the project
   print("\nLifetime tallies: \n")
   lifetime_tallies, lifetime_std = calculate_tallies(dispatch_file=gold, rom_meta_xml=rom_meta_xml)
@@ -194,6 +213,10 @@ def main():
   lifetime_tallies.to_csv(csv_path, header=None)
   fig_path = os.path.join(dispatch_folder, args.location+"_lifetime_tallies_elec_synfuels.png")
   plot_lifetime_tallies(lifetime_tallies, lifetime_std, fig_path)
+
+  # Combine tallies
+  if args.combine: 
+    combine_tallies()
   
 
 if __name__ == '__main__':
